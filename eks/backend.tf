64e8239c3005bf7a9f5b612d820d2890b4d51a94
Jayesh Parmar <89792517+jayeshjeh@users.noreply.github.com>
2024-08-24 22:31:16 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -6,13 +6,13 @@ terraform {
       version = "~> 5.49.0"
     }
   }
-  #backend "s3" {
-  #  bucket         = "my-jeh-bucket1"
-  #  region         = "us-east-1"
-   # key            = "eks/terraform.tfstate"
-  #  dynamodb_table = "proj"
-  #  encrypt        = true
-  #}
+  backend "s3" {
+    bucket         = "my-jeh-bucket1"
+    region         = "us-east-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "proj"
+    encrypt        = true
+  }
 }
 
 provider "aws" {
